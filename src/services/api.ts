00722b09a8df75bafb26210785541ae02b973a5a
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2025-11-05 13:55:17 +0100

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8000/api/member";
+const API_BASE_URL = "https://bpmiwebsitebackend.watchdoglogisticsng.com/api/member";
 
 export const eventsApi = {
   getUpcomingEvents: async () => {
